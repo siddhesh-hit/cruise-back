2f0c3f79f5c05d860c6791f1817b4f49872c899f
siddhesh-hit <siddhesh@handsintechnology.com>
2023-09-26 10:12:36 +0530

    socketio

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // external libraries
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
 // internal libraries
-const { sequelize } = require("./models");
+const { sequelize, MapUserEvent } = require("./models");
 const userCredRoute = require("./routes/user.credentials.routes");
 const userDetailRoute = require("./routes/user.details.routes");
 const eventRoute = require("./routes/user.events.routes");
@@ -15,9 +17,72 @@ const mapRoute = require("./routes/user.maps.routes");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server);
 
 // Serve static files from the "public" directory
 app.use(express.static("uploads"));
+
+// socket io
+io.on("connection", (socket) => {
+  console.log("connected to socket io");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("locationUpdate", async (data) => {
+    console.log(data);
+    try {
+      let exisitingUser = await MapUserEvent.findOne({
+        where: {
+          cred_id: data.cred_id,
+          event_id: data.event_id,
+        },
+      });
+      if (exisitingUser) {
+        await MapUserEvent.update(
+          {
+            current_position: data.current_position,
+            dest_distance: data.dest_distance,
+            dest_arrival: data.dest_arrival,
+            alternative_route: data.alternative_route,
+          },
+          {
+            where: {
+              cred_id: data.cred_id,
+              event_id: data.event_id,
+            },
+          }
+        )
+          .then((updatedLocation) => {
+            console.log("Location updated:", updatedLocation);
+            // Broadcast the updated location to all connected clients except the sender
+            socket.broadcast.emit("locationUpdated", data);
+          })
+          .catch((error) => {
+            console.error("Error updating location:", error);
+          });
+      }
+    } catch (error) {
+      await MapUserEvent.create({
+        cred_id: data.cred_id,
+        event_id: data.event_id,
+        current_position: data.current_position,
+        dest_distance: data.dest_distance,
+        dest_arrival: data.dest_arrival,
+        alternative_route: data.alternative_route,
+      })
+        .then((createdLocation) => {
+          console.log("Location saved:", createdLocation);
+          // Broadcast the updated location to all connected clients except the sender
+          socket.broadcast.emit("locationUpdated", data);
+        })
+        .catch((error) => {
+          console.error("Error saving location:", error);
+        });
+    }
+  });
+});
 
 // syncing the database
 sequelize
@@ -34,6 +99,6 @@ app.use("/api/post", postRoute);
 app.use("/api/map", mapRoute);
 
 // listening
-app.listen("5123", () => {
+server.listen("5123", () => {
   console.log("Server listening to 5123");
 });
